633b03fe6b8ee611a77956dacfa66f2d668c2d73
Шаповалов Вячеслав Борисович <Werdiktbank@yandex.ru>
2022-09-11 20:43:00 +0300

    add

--- lionic.js
+++ lionic.js
@@ -48,8 +48,6 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 });
 
-
-
 // Scroll по меню ******************************************************
 
 $(document).ready(function () {
@@ -65,11 +63,7 @@ $(document).ready(function () {
   });
 });
 
-
 // Scroll fixed header ******************************************************
-
-
-
 
 // $(window).scroll(function () {
 //   var sc = $(window).scrollTop()
@@ -79,3 +73,39 @@ $(document).ready(function () {
 //       $("#header-sroll").removeClass("small")
 //   }
 // });
+
+function setCursorPosition(pos, e) {
+  e.focus();
+  if (e.setSelectionRange) e.setSelectionRange(pos, pos);
+  else if (e.createTextRange) {
+    var range = e.createTextRange();
+    range.collapse(true);
+    range.moveEnd("character", pos);
+    range.moveStart("character", pos);
+    range.select();
+  }
+}
+
+function mask(e) {
+  //console.log('mask',e);
+  var matrix = this.placeholder, // .defaultValue
+    i = 0,
+    def = matrix.replace(/\D/g, ""),
+    val = this.value.replace(/\D/g, "");
+  def.length >= val.length && (val = def);
+  matrix = matrix.replace(/[_\d]/g, function (a) {
+    return val.charAt(i++) || "_";
+  });
+  this.value = matrix;
+  i = matrix.lastIndexOf(val.substr(-1));
+  i < matrix.length && matrix != this.placeholder
+    ? i++
+    : (i = matrix.indexOf("_"));
+  setCursorPosition(i, this);
+}
+window.addEventListener("DOMContentLoaded", function () {
+  var input = document.querySelector("#phone");
+  input.addEventListener("input", mask, false);
+  input.focus();
+  setCursorPosition(3, input);
+});

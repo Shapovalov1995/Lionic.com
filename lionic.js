a3615cfd9adef66522547fad616098131420188b
Шаповалов Вячеслав Борисович <Werdiktbank@yandex.ru>
2022-09-12 15:38:42 +0300

    add code

--- lionic.js
+++ lionic.js
@@ -86,26 +86,64 @@ function setCursorPosition(pos, e) {
   }
 }
 
-function mask(e) {
-  //console.log('mask',e);
-  var matrix = this.placeholder, // .defaultValue
-    i = 0,
-    def = matrix.replace(/\D/g, ""),
-    val = this.value.replace(/\D/g, "");
-  def.length >= val.length && (val = def);
-  matrix = matrix.replace(/[_\d]/g, function (a) {
-    return val.charAt(i++) || "_";
-  });
-  this.value = matrix;
-  i = matrix.lastIndexOf(val.substr(-1));
-  i < matrix.length && matrix != this.placeholder
-    ? i++
-    : (i = matrix.indexOf("_"));
-  setCursorPosition(i, this);
-}
-window.addEventListener("DOMContentLoaded", function () {
-  var input = document.querySelector("#phone");
+// function mask(e) {
+//   //console.log('mask',e);
+//   var matrix = this.placeholder, // .defaultValue
+//     i = 0,
+//     def = matrix.replace(/\D/g, ""),
+//     val = this.value.replace(/\D/g, "");
+//   def.length >= val.length && (val = def);
+//   matrix = matrix.replace(/[_\d]/g, function (a) {
+//     return val.charAt(i++) || "_";
+//   });
+//   this.value = matrix;
+//   i = matrix.lastIndexOf(val.substr(-1));
+//   i < matrix.length && matrix != this.placeholder
+//     ? i++
+//     : (i = matrix.indexOf("_"));
+//   setCursorPosition(i, this);
+// }
+// window.addEventListener("DOMContentLoaded", function () {
+//   var input = document.querySelector("#phone");
+//   input.addEventListener("input", mask, false);
+//   input.focus();
+//   setCursorPosition(3, input);
+// });
+
+
+window.addEventListener("DOMContentLoaded", function() {
+  [].forEach.call( document.querySelectorAll('.tel'), function(input) {
+  var keyCode;
+  function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___-__-__",
+          i = 0,ы
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          new_value = matrix.replace(/[_\d]/g, function(a) {
+              return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+          });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+          i < 5 && (i = 3);
+          new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+          function(a) {
+              return "\\d{1," + a.length + "}"
+          }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5)  this.value = ""
+  }
+
   input.addEventListener("input", mask, false);
-  input.focus();
-  setCursorPosition(3, input);
+  input.addEventListener("focus", mask, false);
+  input.addEventListener("blur", mask, false);
+  input.addEventListener("keydown", mask, false)
+
+});
+
 });

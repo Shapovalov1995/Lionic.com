18691e3cf259cab5daf8b6f137f7331a89a999c2
Шаповалов Вячеслав Борисович <Werdiktbank@yandex.ru>
2022-09-14 03:08:28 +0300

    add code

--- lionic.js
+++ lionic.js
@@ -110,40 +110,94 @@ function setCursorPosition(pos, e) {
 //   setCursorPosition(3, input);
 // });
 
-
-window.addEventListener("DOMContentLoaded", function() {
-  [].forEach.call( document.querySelectorAll('.tel'), function(input) {
-  var keyCode;
-  function mask(event) {
+window.addEventListener("DOMContentLoaded", function () {
+  [].forEach.call(document.querySelectorAll(".tel"), function (input) {
+    var keyCode;
+    function mask(event) {
       event.keyCode && (keyCode = event.keyCode);
       var pos = this.selectionStart;
       if (pos < 3) event.preventDefault();
       var matrix = "+7 (___) ___-__-__",
-          i = 0,ы
-          def = matrix.replace(/\D/g, ""),
-          val = this.value.replace(/\D/g, ""),
-          new_value = matrix.replace(/[_\d]/g, function(a) {
-              return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-          });
+        i = 0,
+        ы;
+      (def = matrix.replace(/\D/g, "")),
+        (val = this.value.replace(/\D/g, "")),
+        (new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+        }));
       i = new_value.indexOf("_");
       if (i != -1) {
-          i < 5 && (i = 3);
-          new_value = new_value.slice(0, i)
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i);
       }
-      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-          function(a) {
-              return "\\d{1," + a.length + "}"
-          }).replace(/[+()]/g, "\\$&");
+      var reg = matrix
+        .substr(0, this.value.length)
+        .replace(/_+/g, function (a) {
+          return "\\d{1," + a.length + "}";
+        })
+        .replace(/[+()]/g, "\\$&");
       reg = new RegExp("^" + reg + "$");
-      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-      if (event.type == "blur" && this.value.length < 5)  this.value = ""
-  }
+      if (
+        !reg.test(this.value) ||
+        this.value.length < 5 ||
+        (keyCode > 47 && keyCode < 58)
+      )
+        this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5) this.value = "";
+    }
 
-  input.addEventListener("input", mask, false);
-  input.addEventListener("focus", mask, false);
-  input.addEventListener("blur", mask, false);
-  input.addEventListener("keydown", mask, false)
-
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+  });
 });
 
+//  ************
+
+// $(document).ready(function() {
+//   $('#text').click(function() {
+//     var val = $('#input').val();
+//     if(val != '') {
+//       if(/[^[0-9]/.test(val)){
+//         var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+//         if(pattern.test(val)){
+//           $('#valid').text('E-mail введен верно');
+//         } else {
+//           $('#valid').text('Не верно введен e-mail');
+//         }
+//       } else {
+//         if(val.length >= 10) {
+//           $('#valid').text('Телефон введен верно');
+//         } else {
+//           $('#valid').text('Не верно введен номер телефона');
+//         }
+//       }
+//     } else {
+//       $('#valid').text('Поле input не должно быть пустым');
+//     }
+//   });
+// });
+
+document.ready(function () {
+  var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
+  var mail = $("#mail");
+
+  mail.blur(function () {
+    if (mail.val() != "") {
+      if (mail.val().search(pattern) == 0) {
+        $("#valid").text("Подходит");
+        $("#submit").attr("disabled", false);
+        mail.removeClass("error").addClass("ok");
+      } else {
+        $("#valid").text("Не подходит");
+        $("#submit").attr("disabled", true);
+        mail.addClass("ok");
+      }
+    } else {
+      $("#valid").text("Поле e-mail не должно быть пустым!");
+      mail.addClass("error");
+      $("#submit").attr("disabled", true);
+    }
+  });
 });
